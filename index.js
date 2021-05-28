d785d2f544cae34dd00ff3fe6050c719a6f75ed5
SaraCantil <>
2021-05-28 00:34:56 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+var num1 = 30; 
+var num2 = 50; 
+
+if (num1 > num2){
+  console.log(num1 + " é maior que " + num2);
+} 
+else if (num1 < num2) {
+  console.log(num1 + " é menor que " + num2); 
+     }
+else {
+  console.log ("os numeros sao iguais")
+} 
+
+// FAÇA UM PROGRAMA QUE LEIA DOIS NUMEROS E IMPRIMA QUAL É O MAIOR//
+
+
+
+
+ 
